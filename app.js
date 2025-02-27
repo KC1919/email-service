c482a0dafb7ba4f6cbf76643380664e2f34128f7
Kunal Chandra <kunal.chandra1900@gmail.com>
2025-02-28 00:55:40 +0530

    add dotenv

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config({ path: 'config/.env' });
 import EmailRouter from './routes/email_routes.js';
 const app = express();
 app.use(express.json());
--- service/email_service.js
+++ service/email_service.js
@@ -1,5 +1,6 @@
-import nodemailer from 'nodemailer'
-
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config({ path: 'config/.env' });
 class EmailService {
 
     constructor(parameters) {
@@ -8,8 +9,8 @@ class EmailService {
             port: 587,
             secure: false,
             auth: {
-                user: "9db955bee28213",
-                pass: "23a6fe26e99824"
+                user: process.env.USER,
+                pass: process.env.PASS
             },
         });
     }
@@ -17,8 +18,8 @@ class EmailService {
     sendEmail = async (subject, body) => {
         try {
             const info = await this.transporter.sendMail({
-                from: '"Kunal Foo Koch 👻" <kunal.chandra1906@gmail.com>', // sender address
-                to: "kunal.chandra1900@gmail.com", // list of receivers
+                from: `"Kunal Foo Koch 👻" <${process.env.FROM_EMAIL}>`, // sender address
+                to: `${process.env.TO_EMAIL}`, // list of receivers
                 subject: subject, // Subject line
                 text: body, // plain text body
                 html: "<b>Hello world?</b>", // html body
